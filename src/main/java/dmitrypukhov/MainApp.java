34a7f8d55a2afd0a2605469fe7b3e1b2f3a99853
INRIX-dmitry-pukhov <dmtiry.puhov@aplana.com>
2016-12-05 14:54:37 +0300

    Empty Java project created

---
+++ src/main/java/dmitrypukhov/MainApp.java
@@ -0,0 +1,18 @@
+package dmitrypukhov;
+
+
+
+/**
+ * The Application
+ */
+public class MainApp {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     */
+    public static void main(String... args) throws Exception {
+        System.out.println("Test")
+    }
+
+}
+
